154078b0189518f5130c9403370ea0abaf0d3ec5
Yauheni Fiodarau <yauheni_fiodarau@epam.com>
2019-01-22 01:09:22 +0300

    Gmail Smoke test

---
+++ module_5/src/test/java/com/epam/atm/pages/GMailAbstractPage.java
@@ -0,0 +1,57 @@
+package com.epam.atm.pages;
+
+import com.epam.atm.singleton.WebDriverSingleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GMailAbstractPage {
+    protected WebDriver webDriver;
+    protected WebDriverWait driverWait;
+    private static final long SLEEP_TIMEOUT = 5000L;
+
+    public GMailAbstractPage() {
+        this.webDriver = WebDriverSingleton.getWebDriverInstance();
+        driverWait = new WebDriverWait(webDriver, SLEEP_TIMEOUT);
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public boolean isElementPresent(By locator) {
+        return webDriver.findElements(locator).size() > 0;
+    }
+
+    public void highlightElement(WebElement element) {
+        String bg = element.getCssValue("backgroundColor");
+        JavascriptExecutor js = ((JavascriptExecutor) webDriver);
+        js.executeScript("arguments[0].style.backgroundColor = '" + "lightgreen" + "'", element);
+        try {
+            Thread.sleep(600);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+    }
+
+    public void waitingForAppearanceElement(WebElement element) {
+        driverWait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void clickOnElement(WebElement element) {
+        highlightElement(element);
+        new Actions(webDriver).click(element).build().perform();
+    }
+
+    public void typeInElement(WebElement element, String text) {
+        highlightElement(element);
+        new Actions(webDriver).sendKeys(element, text).build().perform();
+    }
+
+    public Actions getActions(){
+        return new Actions(webDriver);
+    }
+}
---
+++ module_5/src/test/java/com/epam/atm/pages/GMailLoginPage.java
@@ -0,0 +1,34 @@
+package com.epam.atm.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class GMailLoginPage extends GMailAbstractPage {
+
+    @FindBy(css = "input[type='email']")
+    private WebElement emailInput;
+
+    @FindBy(css = "div#identifierNext")
+    private WebElement nextBtn;
+
+    @FindBy(css = "input[type='password']")
+    private WebElement pwdInput;
+
+    @FindBy(css = "div#passwordNext")
+    private WebElement signInBtn;
+
+    public GMailLoginPage enterAccountName(String accountName){
+        waitingForAppearanceElement(emailInput);
+        typeInElement(emailInput, accountName);
+        clickOnElement(nextBtn);
+        return this;
+    }
+
+    public GMailBoxPage enterPassword(String pwd){
+        waitingForAppearanceElement(pwdInput);
+        typeInElement(pwdInput, pwd);
+        clickOnElement(signInBtn);
+        return new GMailBoxPage();
+    }
+
+}
